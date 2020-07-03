b8eafbbd19bafbe28a221ba9862aa0cd1a5a307b
banda <banda@DESKTOP-4R5VOV9>
2020-07-03 23:59:42 +0530

    Ex1

---
+++ Milestone1/src/com/wipro/flowstatements/Ex1.java
@@ -0,0 +1,18 @@
+package com.wipro.flowstatements;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		n=Integer.parseInt(args[0]);
+		if(n==0)
+			System.out.println("Zero");
+		else if(n>0)
+			System.out.println("Positive");
+		else
+			System.out.println("Negative");
+	
+	}
+
+}
